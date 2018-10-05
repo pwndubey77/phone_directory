070ef7eba3d689c3880453e947d04c5078c99be7
pwndubey77 <pwndubey77@gmail.com>
2018-10-05 22:27:22 +0530

    codebase -cleanupimport removal

--- src/App.js
+++ src/App.js
@@ -5,8 +5,17 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div >
+      <div>
+        {/* <div className="header">
         Phone Directory
+      </div>
+        <button>Add</button>
+      <div>
+        <span> Name</span><br/>
+        <span> Phone</span>
+      </div> */}
+      <label htmlFor="name" >Name: </label>
+      <input id="name" type="text" placeholder="Type Here" defaultValue="pk"/>
       </div>
     );
   }
