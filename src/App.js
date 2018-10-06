273e31d662a4fdd4920a03bb97e9bff9748fc4d5
pwndubey77 <pwndubey77@gmail.com>
2018-10-06 13:51:32 +0530

    Created 'Header' Component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 //import './App.css';
+import Header from './Header';
 
 class App extends Component {
   render() {
+    // let x1  = 6;
+    // let y1 = 7;
+    // let firstname = "Pawan";
+    // let lastName = " Dubey!";
     return (
+
       <div>
-        {/* <div className="header">
-        Phone Directory
-      </div>
-        <button>Add</button>
+        <Header/>
+      <button>Add</button>
       <div>
-        <span> Name</span><br/>
-        <span> Phone</span>
-      </div> */}
-      <label htmlFor="name" >Name: </label>
-      <input id="name" type="text" placeholder="Type Here" defaultValue="pk"/>
+         <span> Name</span><br/>
+         <span> Phone</span>
       </div>
+      </div>
+      
+      // <div>
+      //   {
+      //     /* <div className="header">
+      //   Phone Directory
+      // </div>
+      
+         
+      // {/* <label htmlFor="name" >Name: </label>
+      // <input id="name" type="text" placeholder="Type Here" defaultValue={x1+y1}/>
+      // <p>Hello {firstname + lastName} </p> */}
+      // </div>
+
+        // <div>
+        //   <p>ReactJs</p>
+        // </div>
+      
+        // React.createElement("div",{id: "module"},
+        //   React.createElement("p",null,"ReactJs") )
+    
     );
   }
 }
