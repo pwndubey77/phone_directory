4cb90922f7265571fedeae9a1d045c59c9fbd6a1
pwndubey77 <pwndubey77@gmail.com>
2018-10-14 19:36:38 +0530

    check till props

--- src/Header.js
+++ src/Header.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import './Header.css'
+import './Header.css';
 
-
-const Header = function(){
-    const headerStyle = {textAlign: 'center', padding: 20, background: '#000', color: '#fff',textTransform: 'upperCase'}
-    return(
+const Header = function (props) {
+    return (
         <div className="header">
-            Phone Directory
+            {props.heading}
         </div>
     )
 }
-
-// class Header extends Component{
-//     render(){
-//         return(
-//             <div className="header">
-//                         Phone Directory
-//             </div>
-//         )
-//     }
-// }
-
-
 
 export default Header;
